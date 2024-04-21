0b21a7fe02313705659765f411598d4902033ec1
Dwaipayan Mistry <dwaip.mistry@gmail.com>
2024-04-21 21:39:53 +0530

    error with route(prisma issues), so delete the route.ts

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+// import { PrismaClient } from "@repo/db/client";
 
 
 export default function Page(): JSX.Element {
+  // const client = new PrismaClient()
   return (
-<>hi 
+<>
 <div className="bg-red-200">user APP</div>
 </>
   );
